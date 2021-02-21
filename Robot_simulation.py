886970820a44a2f63b2f00d58b054482cb6d8458
Jaxon <GidntSquia@gmail.com>
2021-02-20 20:24:21 -0500

    Fixed errpr with tournaments not setting winner correctly

--- Robot_simulation.py
+++ Robot_simulation.py
@@ -18,21 +18,23 @@ Epost = Robot("Epost", 0.33, 500, 0)
 Items = Robot("Items", 0.7, 1000, 0)
 
 
-# Battles robot1 and robot2. Returns False if robot1 wins and True if robot2 wins.
+# Battles robot1 and robot2. Returns True if robot1 wins and False if robot2 wins.
 def battle(robot1, robot2):
     if random.randint(1, 100) < robot1.consistency * 100:
         points_scored1 = robot1.points
     else:
         points_scored1 = robot1.minimum_points
     
+    
     if random.randint(1, 100) < robot2.consistency * 100:
         points_scored2 = robot2.points
     else:
         points_scored2 = robot2.minimum_points
-
+    print(robot1.name + " points: " + str(points_scored1))
+    print(robot2.name + " points: " + str(points_scored2))
     if points_scored1 == points_scored2:
         return("Tie")
-    return(points_scored1 < points_scored2)
+    return(points_scored1 > points_scored2)
  
 winners = []
 losers = []
@@ -42,11 +44,11 @@ def do_tournament_between(robots):
     for robot in range(0, len(robots), 2):
         # Adds winning robot to the winning list and losers to the losers list.
         if battle(robots[robot], robots[robot + 1]):
-            winners.append(robots[robot + 1])
-            losers.append(robots[robot])
-        else:
             winners.append(robots[robot])
             losers.append(robots[robot + 1])
+        else:
+            winners.append(robots[robot + 1])
+            losers.append(robots[robot])
     if battle(winners[0], winners[1]):
         return(winners[0].name)
     else:
@@ -57,6 +59,7 @@ def do_tournament_between(robots):
 def main():
     print("Starting code!")
     print()
+    
     for i in range(0,5):
         print("The winner of the tournament: " + do_tournament_between(robots))
         
